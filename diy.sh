85ca6f7a61ef811449c278082042c877dcad230f
Taki <nbjwl@live.com>
2020-03-27 14:29:46 +0800

    add kcptun

--- diy.sh
+++ diy.sh
@@ -7,3 +7,8 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+mkdir package/community
+pushd package/community
+
+git clone https://github.com/kuoruan/luci-app-kcptun.git
